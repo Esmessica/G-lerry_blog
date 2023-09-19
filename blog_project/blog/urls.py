d0e96c74e8f80b5c769afb13e07430ba29d5f4a2
Anna Lorencová <a.lorencov@seznam.cz>
2023-09-19 22:21:48 +0200

    CRUD in views created

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit', views.PostUpdateView.as_view(), name='post_edit'),
+
 
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from blog.models import Post, Comments
-from django.views.generic import (TemplateView, ListView)
+from blog.forms import PostForm, CommentsForm
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 
 # Create your views here.
+
 
 class AboutView(TemplateView):
     template_name = 'about.html'
@@ -21,3 +26,30 @@ class PostListView(ListView):
     --lte=less than or equal to current tiem and order them based by curent date (the dash means DESC order
     
     """
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+
+    login_url = 'login/'
+    redirect_field_name = 'blog/post_detail.html'           # redirect to detail view
+    form_class = PostForm
+    # mixin require those above
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+
+    login_url = 'login/'
+    redirect_field_name = 'blog/post_detail.html'  # redirect to detail view
+    form_class = PostForm
+    # mixin require those above
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
