1857892593b14468e8921e37c04da696dcf19f9d
Anna Lorencová <a.lorencov@seznam.cz>
2023-09-19 22:34:19 +0200

    CRUD in views.py created

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new', views.CreatePostView.as_view(), name='post_new'),
     path('post/<int:pk>/edit', views.PostUpdateView.as_view(), name='post_edit'),
-
+    path('post/<int:pk/delete>', views.PostDeleteView.as_view(), name='post_delete'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list')
 
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from blog.models import Post, Comments
 from blog.forms import PostForm, CommentsForm
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 
@@ -17,7 +16,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
 
     """
     
@@ -51,5 +50,19 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
+
     model = Post
     success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+
+    login_url = 'login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    """
+    Creates query to make sure those posts does not have any published date
+    """
