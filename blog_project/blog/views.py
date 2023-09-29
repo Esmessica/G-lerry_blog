66a2aebe1146292b89a0590c345a6db586f2a105
Anna Lorencová <a.lorencov@seznam.cz>
2023-09-29 21:38:33 +0200

    admin link added for superusers, people without login can add comments

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -104,7 +104,7 @@ def post_publish(request, pk):
     return redirect('post_detail', pk=pk)
 
 
-@login_required
+
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == 'POST':
